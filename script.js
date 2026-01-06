44d1881604af415cc00cb241087e42467a70206f
Suryakartheek Chinninnigari <140693086+suryakartheek@users.noreply.github.com>
2026-01-07 00:29:11 +0530

    Update generative model to 'gemini-2.0-flash'

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const API_KEY = "AIzaSyCCK0QLYhwqSJqjMaiJ2ejGH2PUbkc5zyw"; 
 
 const genAI = new GoogleGenerativeAI(API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 async function sendMessage() {
     const input = document.getElementById('user-input');
