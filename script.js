40e8b76d553a8112269d04c704121f6471c9a6ed
Suryakartheek Chinninnigari <140693086+suryakartheek@users.noreply.github.com>
2026-01-09 22:32:02 +0530

    Update API key in script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const API_KEY = "AIzaSyAzzta9m0mBy2EFiCFyrPuQ1DKOEbYd-uw"; 
+const API_KEY = "AIzaSyAugfjEFdXCxnx3KIbXDFUWrlLujHSL5xk"; 
 const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({ 
     model: "gemini-2.5-flash",
